33413d6442d9f46bce1908886cfffd587b0d3ad4
berkayoztunc <mailberkayoztunc@gmail.com>
2026-04-04 01:24:23 +0300

    chore: bump version to v0.1.1

--- Formula/orquestra-cli.rb
+++ Formula/orquestra-cli.rb
@@ -1,16 +1,16 @@
 class OrquestraCli < Formula
   desc "CLI for interacting with Solana programs via orquestra.dev"
   homepage "https://github.com/berkayoztunc/orquestra-cli"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/berkayoztunc/orquestra-cli/releases/download/v#{version}/orquestra-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "eb05d07292a4ab035ec2860c43037fe915807aee360c22203ab0567fe3e04a81"
+      sha256 "0ff6482e4a015ee17f5e122d1fc6a93ded518042c2646497ba9db15e6f870fed"
     else
       url "https://github.com/berkayoztunc/orquestra-cli/releases/download/v#{version}/orquestra-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "977a773ffc791ccda912f07390c2b9e721a6f004f6ceb4eef1b0af293c0d2343"
+      sha256 "1f7559467f11589b4a7b5b253b28a0ab942de750d23a106706ff83f0db8ef617"
     end
   end
 
